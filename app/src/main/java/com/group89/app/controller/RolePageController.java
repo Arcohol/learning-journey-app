4e475eb9a11f51c981fc80ff3e64d3460b3fcf94
Tianyi Yang <yangty02@163.com>
2023-05-14 18:33:36 +0800

    Implement RolePage

---
+++ app/src/main/java/com/group89/app/controller/RolePageController.java
@@ -0,0 +1,25 @@
+package com.group89.app.controller;
+
+import com.group89.app.model.RoleRecordTableModel;
+import com.group89.app.model.entity.RoleRecord;
+import com.group89.app.view.comp.tablepage.RolePage;
+
+public class RolePageController extends AbstractTablePageController<RoleRecord, RolePage>{
+  public RolePageController(RolePage page) {
+    super(page, "roles.json", RoleRecord[].class, RoleRecord.class);
+
+    init();
+  }
+
+  protected void init() {
+    super.init();
+    
+    query();
+  }
+
+  @Override
+  protected void query() {
+    model = new RoleRecordTableModel(list);
+    view.getTable().setModel(model);
+  }
+}
