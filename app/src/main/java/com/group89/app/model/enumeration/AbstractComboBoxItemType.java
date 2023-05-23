9d0cf42259e174fe7d563dbfe96750ddd5760d15
He Zhu <64079920+zhuhe6644@users.noreply.github.com>
2023-05-23 23:23:20 +0800

    Better implementation of valuesWithoutAll()
    Better implementation of AbstractComboBoxItemType.valuesWithoutAll()

--- app/src/main/java/com/group89/app/model/enumeration/AbstractComboBoxItemType.java
+++ app/src/main/java/com/group89/app/model/enumeration/AbstractComboBoxItemType.java
@@ -1,18 +1,21 @@
 package com.group89.app.model.enumeration;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public abstract class AbstractComboBoxItemType<T> {
   public abstract T[] values();
 
   public abstract T getItemAll();
 
+  @SuppressWarnings("unchecked")
   public T[] valuesWithoutAll() {
     T[] values = values();
-    @SuppressWarnings("unchecked")
-    T[] valuesWithoutAll =
-        (T[]) Array.newInstance(values.getClass().getComponentType(), values.length - 1);
-    System.arraycopy(values, 1, valuesWithoutAll, 0, values.length - 1);
-    return valuesWithoutAll;
+    List<T> valuesWithoutAll = new LinkedList<>(Arrays.asList(values));
+    valuesWithoutAll.remove(getItemAll());
+    return valuesWithoutAll
+        .toArray((T[]) Array.newInstance(values.getClass().getComponentType(), 0));
   }
 }
